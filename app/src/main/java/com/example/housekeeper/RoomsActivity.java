394178170c2a9f2c5e41aa92f7bcf2943648f899
naitsircarm <altric@usi.ch>
2023-09-28 09:46:23 +0200

    RoomsActivity.java

--- app/src/main/java/com/example/housekeeper/RoomsActivity.java
+++ app/src/main/java/com/example/housekeeper/RoomsActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
 import android.widget.Toast;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.widget.EditText;
@@ -16,34 +19,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RoomsActivity extends AppCompatActivity {
 
+
+    private Button status;
+    private Button logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rooms);
 
-//        button = (Button) findViewById(R.id.Status);
-//
-//        // Setting onClick behavior to the button
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                // Initializing the popup menu and giving the reference as current context
-//                PopupMenu popupMenu = new PopupMenu(MainActivity.this, button);
-//
-//                // Inflating popup menu from popup_menu.xml file
-//                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
-//                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-//                    @Override
-//                    public boolean onMenuItemClick(MenuItem menuItem) {
-//                        // Toast message on menu item clicked
-//                        Toast.makeText(MainActivity.this, "You Clicked " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
-//                        return true;
-//                    }
-//                });
-//                // Showing the popup menu
-//                popupMenu.show();
-//            }
-//        });
+        status = findViewById(R.id.Status);
+        logout = findViewById(R.id.logout);
+
+        // Setting onClick behavior to the button
+        status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Initializing the popup menu and giving the reference as current context
+                PopupMenu popupMenu = new PopupMenu(RoomsActivity.this, status);
+
+                // Inflating popup menu from popup_menu.xml file
+                popupMenu.getMenuInflater().inflate(R.menu.dropdown_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        // Toast message on menu item clicked
+                        Toast.makeText(RoomsActivity.this, "You Clicked " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+                // Showing the popup menu
+                popupMenu.show();
+            }
+        });
+
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RoomsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
